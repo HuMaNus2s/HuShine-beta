7e042fe9d03c05c8e13934f3621f459d4aa80050
HuMaNus@s <73073973+HuMaNus2s@users.noreply.github.com>
2024-03-17 00:44:55 +0300

    half-adaptive slide module
    Added a half-adaptive slide module (The movement needs to be modified)

---
+++ js/slide.js
@@ -0,0 +1,42 @@
+var slideIndex = 0;
+var timer; // Добавляем переменную для хранения таймера
+showSlides();
+
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+    resetTimer(); // Добавляем вызов функции сброса таймера
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+    resetTimer(); // Добавляем вызов функции сброса таймера
+}
+
+function resetTimer() {
+    clearTimeout(timer); // Очищаем предыдущий таймер
+    timer = setTimeout(function () {
+        plusSlides(1); // Вызываем функцию для автоматического переключения слайдов
+    }, 3000); // Устанавливаем новый таймер с начальным значением 3 секунды (3000 миллисекунд)
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    } else if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides[slideIndex].style.display = "block";
+    dots[slideIndex].className += " active";
+}
+
+// Запускаем таймер для автоматического переключения слайдов
+resetTimer();
